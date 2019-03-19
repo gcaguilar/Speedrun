6a765fa744aab1b28ad7ae680d52d0dc2bda6b70
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2019-03-19 21:17:52 +0100

    Delete cache dependencies

--- mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/di/modules.kt
+++ mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/di/modules.kt
@@ -1,6 +1,5 @@
 package com.gcaguilar.android.speedrun.ui.di
 
-import androidx.room.Room
 import com.gcaguilar.android.speedrun.data.BufferooDataRepository
 import com.gcaguilar.android.speedrun.data.browse.interactor.GetBufferoos
 import com.gcaguilar.android.speedrun.data.executor.JobExecutor
@@ -11,39 +10,24 @@ import com.gcaguilar.android.speedrun.data.source.BufferooDataStore
 import com.gcaguilar.android.speedrun.data.source.BufferooDataStoreFactory
 import com.gcaguilar.android.speedrun.remote.BufferooRemoteImpl
 import com.gcaguilar.android.speedrun.remote.BufferooServiceFactory
+import com.gcaguilar.android.speedrun.remote.mapper.BufferooEntityMapper
 import com.gcaguilar.android.speedrun.ui.UiThread
 import com.gcaguilar.android.speedrun.ui.browse.BrowseAdapter
 import com.gcaguilar.android.speedrun.ui.browse.BrowseBufferoosViewModel
-import org.buffer.android.boilerplate.cache.BufferooCacheImpl
-import org.buffer.android.boilerplate.cache.PreferencesHelper
-import org.buffer.android.boilerplate.cache.db.BufferoosDatabase
-import org.buffer.android.boilerplate.cache.mapper.BufferooEntityMapper
 import org.buffer.android.boilerplate.ui.BuildConfig
-import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val applicationModule = module(override = true) {
 
-    single { PreferencesHelper(androidContext()) }
 
-    factory { com.gcaguilar.android.speedrun.remote.mapper.BufferooEntityMapper() }
+    factory { BufferooEntityMapper() }
 
     single { JobExecutor() as ThreadExecutor }
     single { UiThread() as PostExecutionThread }
 
-    single {
-        Room.databaseBuilder(
-                androidContext(),
-                BufferoosDatabase::class.java, "bufferoos.db"
-        )
-                .build()
-    }
-    factory { get<BufferoosDatabase>().cachedBufferooDao() }
-
     factory<BufferooDataStore>("remote") { BufferooRemoteImpl(get(), get()) }
-    factory<BufferooDataStore>("local") { BufferooCacheImpl(get(), get(), get()) }
-    factory { BufferooDataStoreFactory(get("local"), get("remote")) }
+    factory { BufferooDataStoreFactory(get("remote")) }
 
     factory { BufferooEntityMapper() }
     factory { BufferooServiceFactory.makeBuffeooService(BuildConfig.DEBUG) }
