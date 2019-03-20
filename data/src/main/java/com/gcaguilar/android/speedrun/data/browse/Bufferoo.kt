f361d31d021378f4ae69dee6cef059b16667b404
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2019-03-20 20:11:07 +0100

    Delete template files

--- data/src/main/java/com/gcaguilar/android/speedrun/data/browse/Bufferoo.kt
+++
@@ -1,6 +0,0 @@
-package com.gcaguilar.android.speedrun.data.browse
-
-/**
- * Representation for a [Bufferoo] fetched from an external layer data source
- */
-data class Bufferoo(val id: Long, val name: String, val title: String, val avatar: String)
