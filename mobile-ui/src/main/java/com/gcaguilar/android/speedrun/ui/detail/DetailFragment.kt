b7a3cc68c4c7d0a8a097ae3049bd50b3ccf530e8
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2019-03-26 00:50:15 +0100

    Reorder navigation, add conversor time

--- mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/detail/DetailFragment.kt
+++ mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/detail/DetailFragment.kt
@@ -6,23 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.gcaguilar.android.speedrun.data.game.model.Runs
 import com.gcaguilar.android.speedrun.ui.R
 import com.gcaguilar.android.speedrun.ui.detail.ItemDetailState.*
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_detail.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import timber.log.Timber
 
-private const val ARG_PARAM1 = "itemId"
+private const val itemIdParam = "itemId"
+private const val nameParam = "gameName"
+private const val photoUriParam = "photoUri"
+
 
 class DetailFragment : Fragment() {
     private var itemId: String? = null
+    private var gameName: String? = null
+    private var photoUri: String? = null
 
-    private val detailViewModel: DetailViewModel by viewModel()
+    private val detailViewModel: DetailViewModel by sharedViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            itemId = it.getString(ARG_PARAM1)
+            itemId = it.getString(itemIdParam)
+            gameName = it.getString(nameParam)
+            photoUri = it.getString(photoUriParam)
         }
     }
 
@@ -31,12 +41,31 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initializeView()
+    }
+
+    private fun initializeView() {
+        gameName?.let { tv_detail_game_name.text = it }
+        photoUri?.let {
+            context?.let { c ->
+                Glide.with(c)
+                        .load(it)
+                        .centerInside()
+                        .into(im_detail_logo_game)
+            }
+        }
+        fb_detail_play_video.setOnClickListener {
+            detailViewModel.onFloatingClicked()
+        }
+
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        detailViewModel.itemDetailLiveData().observe(this, Observer {
+        detailViewModel.itemDetailLiveData().observe(viewLifecycleOwner, Observer {
             if (it != null) handleDetailState(it)
-        }
-        )
+        })
         detailViewModel.getDetails(itemId)
     }
 
@@ -46,18 +75,25 @@ class DetailFragment : Fragment() {
                 setupSuccessDetailState()
                 handleData(state.data)
             }
-            is Loading -> setupLoadingDetailScreen()
-            is Error -> setupErrorDetailScreen()
+            Loading -> setupLoadingDetailScreen()
+            is Error -> {
+                setupErrorDetailScreen()
+                state.errorMessage?.let {
+                    view?.let { v ->
+                        Snackbar.make(v, state.errorMessage, Snackbar.LENGTH_LONG).show()
+                    } ?: Timber.e("View is null")
+                } ?: Timber.e("Cant recover error message")
+            }
         }
     }
 
     private fun handleData(data: Runs?) {
         data?.let {
             tv_detail_player_name.text = it.playerName
-            tv_detail_player_time.text = it.time
+            tv_detail_player_time.text = detailViewModel.formatDate(it.time)
         }
-
     }
+
 
     private fun setupErrorDetailScreen() {
         pb_detail_loading.visibility = View.GONE
@@ -70,17 +106,20 @@ class DetailFragment : Fragment() {
 
     private fun setupSuccessDetailState() {
         pb_detail_loading.visibility = View.GONE
-
     }
 
+
+
     companion object {
-        fun newInstance(itemId: String) =
+        fun newInstance(itemId: String, name: String, uri: String) =
                 DetailFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, itemId)
+                        putString(itemIdParam, itemId)
+                        putString(nameParam, name)
+                        putString(photoUriParam, uri)
                     }
                 }
 
-        val tag = "DetailFragment"
+        const val tag = "DetailFragment"
     }
 }
--- mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/detail/DetailViewModel.kt
+++ mobile-ui/src/main/java/com/gcaguilar/android/speedrun/ui/detail/DetailViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.gcaguilar.android.speedrun.data.game.interactor.GetRunsUseCase
+import com.gcaguilar.android.speedrun.ui.main.Event
 import io.reactivex.disposables.Disposable
+import org.joda.time.Period.parse
+import org.joda.time.format.PeriodFormatterBuilder
 import timber.log.Timber
 
 class DetailViewModel(
@@ -12,10 +15,16 @@ class DetailViewModel(
 ) : ViewModel() {
 
     private val itemDetailLiveData = MutableLiveData<ItemDetailState>()
+    private val navigateToVideo = MutableLiveData<Event<String>>()
+    private var videoUri: String? = null
     private var disposable: Disposable? = null
 
     fun itemDetailLiveData(): LiveData<ItemDetailState> {
         return itemDetailLiveData
+    }
+
+    fun navigetToVideo(): LiveData<Event<String>> {
+        return navigateToVideo
     }
 
     fun getDetails(gameId: String?) {
@@ -23,9 +32,11 @@ class DetailViewModel(
             itemDetailLiveData.value = ItemDetailState.Loading
             disposable = getRunsUseCase.execute(gameId)
                     .subscribe({
+                        videoUri = it.videoUri
                         itemDetailLiveData.value = ItemDetailState.Success(it)
                     }, {
                         Timber.e(it)
+                        itemDetailLiveData.value = ItemDetailState.Error(it.message)
                     }
                     )
         }
@@ -35,4 +46,24 @@ class DetailViewModel(
         super.onCleared()
         disposable?.dispose()
     }
+
+    fun onFloatingClicked() {
+        videoUri?.let {
+            navigateToVideo.value = Event(it)
+        }
+    }
+
+    fun formatDate(time: String): String {
+        val period = parse(time)
+        val periodFormatter = PeriodFormatterBuilder()
+                .printZeroAlways()
+                .appendHours()
+                .appendSeparator(":")
+                .appendMinutes()
+                .appendSeparator(":")
+                .appendSeconds()
+                .toFormatter()
+        return periodFormatter.print(period)
+    }
+
 }
